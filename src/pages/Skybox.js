e3b60c935a6e90cdfc44b64eb961cce67f249668
Faber-smythe <johann_imbert@yahoo.fr>
2020-09-07 16:27:44 +0200

    fixed issue by adding Math.asb() and added a link to source

--- src/pages/Skybox.js
+++ src/pages/Skybox.js
@@ -1,6 +1,9 @@
+/** --------------------- /
+ *      IMPORTS
+ ** --------------------- */
 import React, {Component} from 'react';
-import $ from 'jquery';
 
+/** ---- import the 6 textures (order doesn't matter) ---- **/
 import textureRt from '../textures/right.jpg';
 import textureLt from '../textures/left.jpg';
 import textureUp from '../textures/up.jpg';
@@ -8,22 +11,26 @@ import textureDn from '../textures/down.jpg';
 import textureBk from '../textures/back.jpg';
 import textureFt from '../textures/front.jpg';
 
+/** ----  ---- **/
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-
+/** --------------------- /
+ *      COMPONENT
+ ** --------------------- */
 export default function Skybox(props){
     let renderer = new THREE.WebGLRenderer({antialias:true});
     renderer.setSize(window.innerWidth, window.innerHeight);
-    $('body').append(renderer.domElement);
+    document.querySelector('body').append(renderer.domElement);
 
     let scene = new THREE.Scene;
 
     let camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 1, 1000);
-    camera.position.set( 0, 0, -1 ); // Camera controls don't work when Z == 0)
+    camera.position.set( 0, 0, 1 ); // Camera controls don't work when Z == 0)
     camera.lookAt(0, 0, 0);
 
-    let geometry = new THREE.BoxGeometry(20, 20, 20);
+    let geometry = new THREE.BoxGeometry(100, 100, 100);
     let materialArray = [];
     let texture_ft = new THREE.TextureLoader().load(textureFt);
     let texture_bk = new THREE.TextureLoader().load(textureBk);
@@ -65,13 +72,15 @@ export default function Skybox(props){
     }
     function rangeZoom(){
         let {x, y, z} = camera.position;
-        let ratioX = x / (x + y + z);
-        let ratioY = y / (x + y + z);
-        let ratioZ = z / (x + y + z);
+        let ratioX = x / (Math.abs(x) + Math.abs(y) + Math.abs(z));
+        let ratioY = y / (Math.abs(x) + Math.abs(y) + Math.abs(z));
+        let ratioZ = z / (Math.abs(x) + Math.abs(y) + Math.abs(z));
+        console.log(ratioX);
+        console.log(x);
         camera.position.set(
-            ratioX*(101-document.querySelector(".zoomRange").value),
-            ratioY*(101-document.querySelector(".zoomRange").value),
-            ratioZ*(101-document.querySelector(".zoomRange").value)
+            ratioX*(201-document.querySelector(".zoomRange").value),
+            ratioY*(201-document.querySelector(".zoomRange").value),
+            ratioZ*(201-document.querySelector(".zoomRange").value)
         );
     }
     function rangeFov(){
@@ -92,13 +101,16 @@ export default function Skybox(props){
         alignItems: 'center'
     }
     return(
-        <h2 style={localStyle}>
+   <>
+       <h2 style={localStyle}>
             Try dragging around, zooming out, or clicking here >&nbsp;
             <span onClick={toggle} style={{cursor:'pointer', color:'Aqua'}}>
                 Toggle Backfacing
             </span>
-            <span style={{marginLeft:'30px'}}>ZOOM</span><input className="zoomRange" type="range" onChange={rangeZoom} defaultValue={100}/>
+            <span style={{marginLeft:'30px'}}>ZOOM</span><input className="zoomRange" type="range" min="10" max="200" onChange={rangeZoom} defaultValue={200}/>
             <span style={{marginLeft:'30px'}}>FOV</span><input className="fovRange" type="range" min="10" max="150" onChange={rangeFov} defaultValue={70}/>
         </h2>
+        <a target="_blank" href="https://github.com/Faber-smythe/js_visualization/blob/master/src/pages/Skybox.js" style={{...localStyle, ...{right:'5vw', marginRight:'0'}}}>SOURCE</a>
+    </>
     )
 }
